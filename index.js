be3eb20a5c5047b5b287bf519393c15441a31d3f
Alex Aguilar <auza920205@gmail.com>
2020-09-29 22:29:22 -0500

    first commit

--- index.js
+++ index.js
@@ -17,33 +17,6 @@ const main = async () => {
 	} catch(error) {
 		console.log(error)
 	}
-	/*try {
-		const usbPort = new SerialPort('/dev/ttyACM0', { baudRate: 115200 })
-		const parser = new Readline()
-	  usbPort.pipe(parser)
-	  parser.on('data', (data) => {
-	  	console.log(data)
-	  	/*let json = JSON.parse(data)
-	    json = {
-	      date: Date.now(),
-	      temperature: Math.round((json.temperature + Number.EPSILON) * 100) / 100,
-	      humidity: Math.round((json.humidity + Number.EPSILON) * 100) / 100,
-	      pressure: Math.round((json.pressure + Number.EPSILON) * 100) / 100,
-	      windSpeed: Math.round((json.windSpeed + Number.EPSILON) * 100) / 100,
-	      windDirection: json.windDirection,
-	      location: {
-	        latitude: json.latitude,
-	        longitude: json.longitude,
-	        altitude: json.altitude
-	      }
-	    }
-	  })
-	  setInterval(() => {
-	  	usbPort.write('hola \n')
-	  }, 5000)
-	} catch(error) {
-		console.log(error)
-	}*/
 }
 
 const mongooseConnect = async () => {
